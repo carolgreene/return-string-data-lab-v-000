dc44e806e0d4b01969b08d5e910046e337e3cbfd
Carol Greene <carolgreene3@gmail.com>
2018-12-29 17:02:06 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,6 +9,11 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+  def description 
+    product = Product.find(params[:id])
+    render plain: product.description 
+  end 
+
 
 
   private
