98ef6966390e7465ae99c12793149131f946bd87
Carol Greene <carolgreene3@gmail.com>
2018-12-29 16:39:01 +0000

    Automatically backed up by Learn

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,4 @@ class Product < ActiveRecord::Base
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
 
-  def inventory_check
-    self.inventory > 0 ? "true" : "false"
-  end 
-end
+  
