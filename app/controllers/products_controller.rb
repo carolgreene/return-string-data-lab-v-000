761f36e9e414708b9b2adbfeda49a5e5bc82b4bc
Carol Greene <carolgreene3@gmail.com>
2018-12-29 16:54:03 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,12 @@
 class ProductsController < ApplicationController
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.create(product_params)
+    redirect_to products_path
+  end
+
 end
