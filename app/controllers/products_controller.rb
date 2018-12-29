ace2ce4826d6996124f6e3cec8adc694b3a9ee92
Carol Greene <carolgreene3@gmail.com>
2018-12-29 16:56:12 +0000

    Automatically backed up by Learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,12 @@ class ProductsController < ApplicationController
     redirect_to products_path
   end
 
+
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :inventory)
+  end
+
 end
